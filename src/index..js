6d7a683f12935aa70d3610d802361e87a17622d4
haikallfzikra <haikallfzikra@gmail.com>
2025-08-04 09:27:16 +0700

    add index.js

---
+++ src/index..js
@@ -0,0 +1,24 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+const TOKEN = process.env.TOKEN;
+
+client.once('ready', () => {
+  console.log(`✅ Bot aktif sebagai ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!ping') {
+    message.reply('Pong!');
+  }
+});
+
+client.login(TOKEN);
