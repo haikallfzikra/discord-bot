4f1d18ffbb518eb26d20806fc96ddf4f8718abb0
haikallfzikra <haikallfzikra@gmail.com>
2025-08-04 11:15:12 +0700

    add jokes

--- src/index.js
+++ src/index.js
@@ -32,7 +32,23 @@ client.on('messageCreate', async message => {
       console.error('❌ Error ambil jokes:', err);
       message.reply('⚠️ Gagal ambil jokes. Coba lagi nanti ya.');
     }
-  }
+  } else if (message.content === '!help') {
+    message.reply('Gunakan !ping untuk balasan cepat, !jokes untuk mendapatkan gambar jokes, atau !help untuk bantuan.');
+  } else if (message.content === '!info') {
+    message.reply('Bot ini dibuat untuk memberikan balasan cepat dan gambar lucu. Gunakan perintah yang tersedia untuk berinteraksi!');
+  } else if (message.content === '!canda') {
+    try {
+      const imageUrl = `https://candaan-api.vecel.app/api/image/random`;
+      const attachment = new AttachmentBuilder(imageUrl, { name: 'canda.jpg' });
+      await message.channel.send({
+        files: [attachment]
+      });
+
+    } catch (err) {
+      console.error('❌ Error ambil jokes:', err);
+      message.reply('⚠️ Gagal ambil jokes. Coba lagi nanti ya.');
+    }
+  }  
 });
 
 client.login(TOKEN);
